52dde54b4eea6b1594c6a113bf622ba0a801590b
Florian Nitschmann <florian.nitschmann@infopark.de>
2013-07-12 12:09:02 +0200

    updated metadata.rb and added default attributes

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+# Default attributes for AWS S3 
+default[:aws_s3][:access_key] = ""
+default[:aws_s3][:access_key_secret] = ""
+default[:aws_s3][:default_bucket] = ""
--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,20 @@
 maintainer 'Florian Nitschmann'
 maintainer_email 'florian.nitschmann@infopark.de'
-description 'File download directly from Amazon AWS S3'
+description 'File download directly from AWS S3'
 version '0.1'
 supports 'ubuntu', '>= 8.04'
 
+attribute 'aws_s3/access_key',
+	:display_name => 'AWS S3 Access Key ID',
+	:description => 'The Access Key ID for AWS S3'
+	:default => ''
+
+attribute 'aws_s3/access_key_secret',
+	:display_name => 'AWS S3 Access Key Secret',
+	:description => 'The AWS S3 Access Key Secret'
+	:default => ''
+
+attribute 'aws_s3/default_bucket',
+	:display_name => 'Default AWS S3 bucket for stored files'
+	:description => 'A default bucket on AWS S3 where files should be downloaded from'
+	:default => ''
