212f9d3b09e196783d1b17c7f963330ed49b16d0
Florian Nitschmann <florian.nitschmann@infopark.de>
2013-07-12 11:41:47 +0200

    created an empty repro with metadata.rb and readme

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer 'Florian Nitschmann'
+maintainer_email 'florian.nitschmann@infopark.de'
+description 'File download directly from Amazon AWS S3'
+version '0.1'
+supports 'ubuntu', '>= 8.04'
+
