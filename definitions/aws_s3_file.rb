bdab74e2571af0ede44ee2a79045da92093b5876
Florian Nitschmann <florian.nitschmann@infopark.de>
2013-07-12 14:10:48 +0200

    first version of aws_s3_file definition finished

--- definitions/aws_s3_file.rb
+++ definitions/aws_s3_file.rb
@@ -13,7 +13,7 @@ define :aws_s3_file, :action => :download do
 			bucket = node[:aws_s3][:default_bucket]			
 		end		
 		file = params[:file]
-		location = params[:location]
+		location = params[:into]
 		destination = params[:as] || File.join(location, file)
 		owner = params[:owner]
 		group = params[:group]
@@ -24,7 +24,24 @@ define :aws_s3_file, :action => :download do
     	action :create
   	end
 
+	  execute "download #{params[:name]} from S3" do
+	    command "s3cmd get -c /root/.s3cfg s3://#{bucket}/#{file} #{destination}"
+	    only_if { Dir.glob(destination).blank? }
+	  end
 
+	  if owner != nil && group != nil 
+	  	bash "change owner for #{file}" do 
+	  		cwd location
+	  		code %(chown -R #{owner}:#{group} #{file})
+	  	end 
+	  end
+
+	  if mode != nil 
+	  	bash "change mode for #{file}" do
+	  		cwd location 
+	  		code %(chmod -R #{mode} #{file})
+	  	end
+	  end
 	end
 
 end 
